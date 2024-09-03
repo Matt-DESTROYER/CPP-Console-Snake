25975733f53de2f0ea9c33dcfcb51ce00c1c43f8
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-09-03 02:41:59 +0000

    Specific build folders

--- src/build.sh
+++ src/build.sh
@@ -9,9 +9,20 @@ cmake ../src
 cmake --build ./
 make
 
+# detect architecture
+arch=$(uname -m)
+if [[ $arch == x86_64* ]]; then
+	arch="x64"
+elif [[ $arch == i*86 ]]; then
+	arch="x32"
+elif  [[ $arch == arm* ]]; then
+	arch="arm"
+fi
+
 # copy the executable to the build directory
-[[ -f ./main ]] && cp -f ./main ../build
-[[ -f ./main.exe ]] && cp -f ./main.exe ../build
+mkdir -p ../build/$OSTYPE-$arch/
+[[ -f ./main ]] && cp -f ./main ../build/$OSTYPE-$arch/
+[[ -f ./main.exe ]] && cp -f ./main.exe ../build/$OSTYPE-$arch/
 
 # clean up temporary directory
 cd ../
