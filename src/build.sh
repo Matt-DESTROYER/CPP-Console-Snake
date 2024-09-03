4a840b64dd6b235db2401852acd9abd0010903be
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-09-03 01:38:10 +0000

    File existance checks

--- src/build.sh
+++ src/build.sh
@@ -8,8 +8,8 @@ cmake --build ./
 make
 
 # copy the executable to the build directory
-[[ -e ./main ]] && cp ./main ../build
-[[ -e ./main.exe ]] && cp ./main.exe ../build
+[[ -f ./main ]] && cp ./main ../build
+[[ -f ./main.exe ]] && cp ./main.exe ../build
 
 # clean up temporary directory
 cd ../
