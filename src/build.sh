032ea3528404f6333ba195b9aaf9de8725626d77
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-09-03 18:07:33 +1000

    Don't just cp executable...

--- src/build.sh
+++ src/build.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
 
-# create a temporary directory
-mkdir ./temp
-cd ./temp
-
-# build the project
-cmake ../src
-cmake --build ./
-make
-
 # detect architecture
 ARCH=$(uname -m)
 if [[ $ARCH == x86_64* ]]; then
 	ARCH="x64"
 elif [[ $ARCH == i*86 ]]; then
 	ARCH="x32"
-elif  [[ $arch == arm* ]]; then
+elif [[ $arch == arm* ]]; then
 	ARCH="arm"
 fi
 
-# copy the executable to the build directory
+# create a build directory (if it doesn't exist)
 mkdir -p ../build/$OSTYPE-$ARCH/
-[[ -f ./main ]] && cp -f ./main ../build/$OSTYPE-$ARCH/
-[[ -f ./main.exe ]] && cp -f ./main.exe ../build/$OSTYPE-$ARCH/
 
-# clean up temporary directory
-cd ../
-rm -r -f ./temp
+# build the project
+cmake ../src
+cmake --build ./
+make
