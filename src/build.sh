75f8ebe54a8d6ec9629b7fcf30241165d332806a
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-09-03 07:37:33 +0000

    Remove debug prints

--- src/build.sh
+++ src/build.sh
@@ -10,22 +10,19 @@ cmake --build ./
 make
 
 # detect architecture
-arch=$(uname -m)
-if [[ $arch == x86_64* ]]; then
-	arch="x64"
-elif [[ $arch == i*86 ]]; then
-	arch="x32"
+ARCH=$(uname -m)
+if [[ $ARCH == x86_64* ]]; then
+	ARCH="x64"
+elif [[ $ARCH == i*86 ]]; then
+	ARCH="x32"
 elif  [[ $arch == arm* ]]; then
-	arch="arm"
+	ARCH="arm"
 fi
 
 # copy the executable to the build directory
-mkdir -p ../build/$OSTYPE-$arch/
-[[ -f ./main ]] && cp -f ./main ../build/$OSTYPE-$arch/
-[[ -f ./main.exe ]] && cp -f ./main.exe ../build/$OSTYPE-$arch/
-
-echo $(ls ./)
-echo $OSTYPE-$arch
+mkdir -p ../build/$OSTYPE-$ARCH/
+[[ -f ./main ]] && cp -f ./main ../build/$OSTYPE-$ARCH/
+[[ -f ./main.exe ]] && cp -f ./main.exe ../build/$OSTYPE-$ARCH/
 
 # clean up temporary directory
 cd ../
